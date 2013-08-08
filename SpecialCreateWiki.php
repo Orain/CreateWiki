5fd8bac89eb7db717a3058f0e4754bdc8eb70137
Kudu <kudu@riseup.net>
2013-08-08 18:07:45 -0400

    Add copyright notice to main file

--- SpecialCreateWiki.php
+++ SpecialCreateWiki.php
@@ -1,4 +1,23 @@
-d<?php
+<?php
+/**
+ * Copyright (C) 2013 Orain, Kudu and contributors
+ *
+ * This file is part of CreateWiki.
+ *
+ * CreateWiki is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * CreateWiki is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CreateWiki.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class SpecialCreateWiki extends SpecialPage {
 	function __construct() {
 		parent::__construct( 'CreateWiki', 'createwiki' );
