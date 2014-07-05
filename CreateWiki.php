eaf96aa911a45b2339eb041cad1933d755da6adb
John Lewis <johnflewis93@gmail.com>
2014-07-05 15:05:10 +0000

    Add hack for now
    TODO - fix this eventually

--- CreateWiki.php
+++ CreateWiki.php
@@ -13,6 +13,7 @@ $wgExtensionCredits['specialpage'][] = array(
 
 $wgAutoloadClasses['SpecialCreateWiki'] = dirname( __FILE__ ) . '/SpecialCreateWiki.php';
 $wgExtensionMessagesFiles['CreateWiki'] = dirname( __FILE__ ) . '/CreateWiki.i18n.php';
+$wgMessagesDirs['CreateWiki'] = dirname( __FILE__) . '/i18n';
 $wgExtensionMessagesFiles['CreateWikiAlias'] = dirname( __FILE__ ) . '/CreateWiki.alias.php';
 $wgSpecialPages['CreateWiki'] = 'SpecialCreateWiki';
 
@@ -23,5 +24,14 @@ $wgLogActionsHandlers['farmer/*'] = 'LogFormatter';
 /**
  * SQL files to be sourced into the created databases.
  */
-$wgCreateWikiSQLfiles = array( "$IP/maintenance/tables.sql" );
 
+$wgCreateWikiSQLfiles = array(
+	"$IP/maintenance/tables.sql",
+	"$IP/extensions/AbuseFilter/abusefilter.tables.sql",
+	"$IP/extensions/AntiSpoof/sql/patch-antispoof.mysql.sql",
+	"$IP/extensions/CentralNotice/CentralNotice.sql",
+	"$IP/extensions/CheckUser/cu_changes.sql",
+	"$IP/extensions/CheckUser/cu_log.sql",
+	"$IP/extensions/CreateWiki/global_block_whitelist.sql",
+	"$IP/extensions/TitleKey/titlekey.sql"
+);
